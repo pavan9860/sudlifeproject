627f983c90295772379b606c23640802e8f190e7
pavan02091992 <68508235+pavan02091992@users.noreply.github.com>
2021-01-25 23:43:48 +0530

    my first complete framework upload in github

---
+++ Sudlife/src/main/java/testsi/testsiproject.java
@@ -0,0 +1,18 @@
+package testsi;
+
+import org.testng.annotations.Test;
+
+public class testsiproject {
+	
+	@Test(enabled=false)
+	public void newsampoorna()
+	{
+	 System.out.println("print project");
+	}
+
+	@Test
+	public void century_plus()
+	{
+		System.out.println("print system");
+	}
+}
